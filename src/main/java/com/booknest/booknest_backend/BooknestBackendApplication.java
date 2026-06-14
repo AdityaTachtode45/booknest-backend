0e06c0deb986fb2df83d84275856c9f1ce70e6a9
Aditya Namdev Tachtode <120795432+AdityaTachtode45@users.noreply.github.com>
2026-06-14 13:40:03 +0530

    Update BooknestBackendApplication.java

--- src/main/java/com/booknest/booknest_backend/BooknestBackendApplication.java
+++ src/main/java/com/booknest/booknest_backend/BooknestBackendApplication.java
@@ -2,12 +2,40 @@ package com.booknest.booknest_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @SpringBootApplication
+@EnableScheduling
 public class BooknestBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooknestBackendApplication.class, args);
 	}
 
+	@Scheduled(fixedRate = 30000)
+	public void callApi() {
+
+		try {
+
+			URL url = new URL(
+				"https://official-joke-api.appspot.com/random_joke"
+			);
+
+			HttpURLConnection connection =
+					(HttpURLConnection) url.openConnection();
+
+			connection.setRequestMethod("GET");
+
+			System.out.println(
+				"Response Code: " + connection.getResponseCode()
+			);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
